2b1d1b8964686c0f74e0adbdbd9014a3bf7d5174
McMurphy-Luo <McMurphy.Luo@gmail.com>
2018-05-29 17:20:40 +0800

    Remove all graphics unit, stop writing a render regine, device to use google's skia as render engine. My job switch to write a control library

---
+++ src/control/Base.h
@@ -0,0 +1,17 @@
+#ifndef MUI_CONTROL_BASE_H_
+#define MUI_CONTROL_BASE_H_
+
+#include "../Config.h"
+#include "./EventEmiter.h"
+
+NAMESPACE_BEGIN
+
+class Base: public EventEmiter {
+
+
+
+};
+
+NAMESPACE_END
+
+#endif // MUI_CONTROL_BASE_H
---
+++ src/control/Button.h
@@ -0,0 +1,12 @@
+#ifndef MUI_CONTROL_BUTTON_
+#define MUI_CONTROL_BUTTON_
+
+#include "../Config.h"
+
+NAMESPACE_BEGIN
+
+
+NAMESPACE_END
+
+
+#endif
